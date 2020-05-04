355af9b90d55eea5d1f675c9be13dbe61fefe4bc
heyifuwhu <heyifuwhu@gmail.com>
2020-05-03 22:52:41 -0400

    Updated by Yifu Jason He

--- core/MonteCarlo.py
+++ core/MonteCarlo.py
@@ -1,10 +1,8 @@
 from core import PayoffClass
 
 class MonteCarlo(object):
-    def __init__(self, payoff_, S_, T, r, sigma, div=0):
+    def __init__(self, payoff_, S_, T_, r_, sigma_, num_of_path_=1000, div=0):
         pass
-
-
 
 
 
