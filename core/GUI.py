4c569648d05b52523c8b5fc9b2f22c47b37e5768
Junone <34847141+junone@users.noreply.github.com>
2020-05-08 14:44:47 -0400

    Update GUI.py

--- core/GUI.py
+++ core/GUI.py
@@ -2,6 +2,7 @@ import tkinter
 from tkinter import *
 from tkinter import ttk
 from Tree_Model_Base import tree_model
+from Black_Schcoles_Models_concrete import *
 class StartPage(object):
     def __init__(self):
         self.root=Tk()
@@ -61,13 +62,14 @@ class CalculatorPage(object):
             self.answ = Label(self.root, text='The result is as follows:')
             self.answ.grid(row=r, columnspan=3)
             r += 1
-            self.bs = Label(self.root)
+            self.BS = Label(self.root)
             #self.mc = Label(self.root)
-            self.bs.grid(row=r, columnspan=2, sticky=E)
+            self.BS.grid(row=r, columnspan=2, sticky=E)
             #self.mc.grid(row=r+1, columnspan=2, sticky=E)
-            Label(self.root, text='Use BS Model: ').grid(row=r, sticky=W)
+            Label(self.root, text='European Option: ').grid(row=r, sticky=W)
             #Label(self.root, text='Use Monte Carlo: ').grid(row=r+1, sticky=W)
             Button(self.root, text='Go Back', command=self.go_startPage).grid(row=r + 1)
+            #Button(self.root, text='Go Back', command=self.go_startPage).pack()
         elif self.modelType=="Monte Carlo":
             self.plist = ['Current Price', 'Strike Price', 'Days to Maturity',
                      'Risk-free Rate', 'Volatility', 'Continuous Dividend Rate','num_of_path']
@@ -91,7 +93,31 @@ class CalculatorPage(object):
             Label(self.root, text='Use Monte Carlo: ').grid(row=r, sticky=W)
             #Label(self.root, text='Use Monte Carlo: ').grid(row=r+1, sticky=W)
             Button(self.root, text='Go Back', command=self.go_startPage).grid(row=r+1)
-        elif self.modelType=="Monte Carlo":
+        elif self.modelType=="Binary tree":
+            self.plist = ['Current Price', 'Strike Price', 'Days to Maturity',
+                     'Risk-free Rate', 'Volatility','Number_of_step']
+            self.elist = [] # for parameters
+            r = 5 # r start from 0 and now is 3
+            for param in self.plist:
+                Label(self.root, text=param).grid(column=0, sticky=W)
+                e = Entry(self.root)
+                e.grid(row=r, column=1, columnspan=2, sticky=W+E)
+                self.elist.append(e)
+                r += 1
+            Button(self.root, text='Calculate',command = self.get_parameter_BT).grid(row=r)
+            r += 1
+            self.answ = Label(self.root, text='The result is as follows:')
+            self.answ.grid(row=r, columnspan=3)
+            r += 1
+            self.EU = Label(self.root)
+            self.AM = Label(self.root)
+            #self.mc = Label(self.root)
+            self.EU.grid(row=r, columnspan=2, sticky=E)
+            self.AM.grid(row=r+1, columnspan=2, sticky=E)
+            Label(self.root, text='European Option : ').grid(row=r, sticky=W)
+            Label(self.root, text='American Option : ').grid(row=r+1, sticky=W)
+            Button(self.root, text='Go Back', command=self.go_startPage).grid(row=r+2)
+
 
     def get_parameter_BS(self):
         vlist = []
@@ -103,10 +129,14 @@ class CalculatorPage(object):
                 answ.config(text='Invalid Input(s). Please input correct parameter(s)', fg='red')
                 e.delete(0, len(e.get()))
                 return 0
-        optionPrice = vlist[0] + vlist[1] + vlist[2] + vlist[3] + vlist[4]
-        print(optionPrice)
+
         self.answ.config(text='The result is as follows:', fg='black')
-        self.bs.config(text=str("%.8f" % (optionPrice/10)))
+        if self.optionType=="Call Option":
+            option=European_Call_BS(vlist[0], vlist[1], vlist[2], vlist[3], vlist[4])
+            self.BS.config(text=str("%.8f" % (option.get_Option_Price())))
+        else:
+            option = European_Put_BS(vlist[0], vlist[1], vlist[2], vlist[3], vlist[4])
+            self.BS.config(text=str("%.8f" % (option.get_Option_Price())))
         #self.mc.config(text=str("%.8f" % (optionPrice*10)))
     def get_parameter_MC(self):
         vlist = []
@@ -119,10 +149,27 @@ class CalculatorPage(object):
                 e.delete(0, len(e.get()))
                 return 0
         optionPrice = vlist[0] + vlist[1] + vlist[2] + vlist[3] + vlist[4]
-        print(optionPrice)
         self.answ.config(text='The result is as follows:', fg='black')
         self.MC.config(text=str("%.8f" % (optionPrice/10)))
         #self.mc.config(text=str("%.8f" % (optionPrice*10)))
+    def get_parameter_BT(self):
+        vlist = []
+        for e in self.elist:
+            try:
+                p = float(e.get())
+                vlist.append(p)
+            except:
+                answ.config(text='Invalid Input(s). Please input correct parameter(s)', fg='red')
+                e.delete(0, len(e.get()))
+                return 0
+        option=tree_model(vlist[0],vlist[1],vlist[2],vlist[3],vlist[4])
+        self.answ.config(text='The result is as follows:', fg='black')
+        if self.optionType=="Call Option":
+            self.EU.config(text=str("%.8f" % (option.European_Binomial_Multiplicative('call', int(vlist[5]), 1.1, 1 / 1.1))))
+            self.AM.config(text=str("%.8f" % (option.American_Binomial_Multiplicative('call', int(vlist[5]), 1.1, 1 / 1.1))))
+        else:
+            self.EU.config(text=str("%.8f" % (option.European_Binomial_Multiplicative('put', int(vlist[5]), 1.1, 1 / 1.1))))
+            self.AM.config(text=str("%.8f" % (option.American_Binomial_Multiplicative('put', int(vlist[5]), 1.1, 1 / 1.1))))
     def go_startPage(self):
         self.root.destroy()
         StartPage()
@@ -131,68 +178,3 @@ class CalculatorPage(object):
 if __name__ == '__main__':
     StartPage()
 
-
-# class Window():
-#     def __init__(self,root):
-#         selt
-#
-#
-#
-#
-#
-# def main():
-#     root=tkinter.TK()
-#     root.title("Option Price Calculator")
-#     window=Window(root)
-#     root.minsize(600, 500)
-#     root.maxsize(600, 500)
-#     root.mainloop()
-
-
-# root = tkinter.Tk() # Create an object
-# root.wm_title('European Option Price Calculator')
-# tkinter.Label(root, text='Please input relevant parameters, '
-#                  'then click "Calculate" button.').grid(row=0, column=0, columnspan=3)
-# tkinter.Label(root, text='by Junyi, MQF @RBS').grid(columnspan=3)
-# cp = tkinter.StringVar()
-# Label(root, text='Option Type').grid(row=2, column=0, sticky=W)
-# Radiobutton(root, text='Call', variable=cp, value='c').grid(row=2, column=1)
-# Radiobutton(root, text='Put', variable=cp, value='p').grid(row=2, column=2)
-#
-# plist = ['Current Price', 'Strike Price', 'Days to Maturity',
-#          'Risk-free Rate', 'Volatility', 'Continuous Dividend Rate', 'MC Iteration']
-# elist = []
-# r = 3 # r start from 0 and now is 3
-# for param in plist:
-#     Label(root, text=param).grid(column=0, sticky=W)
-#     e = Entry(root)
-#     e.grid(row=r, column=1, columnspan=2, sticky=W+E)
-#     elist.append(e)
-#     r += 1
-# Button(root, text='Calculate').grid(row=r)
-# r += 1
-#
-# answ = Label(root, text='The result is as follows:')
-# answ.grid(row=r, columnspan=3)
-# r += 1
-# bs = Label(root)
-# mc = Label(root)
-# bs.grid(row=r, columnspan=2, sticky=E)
-# mc.grid(row=r+1, columnspan=2, sticky=E)
-# Label(root, text='Use BS formula: ').grid(row=r, sticky=W)
-# Label(root, text='Use Monte Carlo: ').grid(row=r+1, sticky=W)
-# root.mainloop()
-
-# def go(*args):  # 处理事件，*args表示可变参数
-#     print(comboxlist.get())  # 打印选中的值
-#
-#
-# win = tkinter.Tk()  # 构造窗体
-# comvalue = tkinter.StringVar()  # 窗体自带的文本，新建一个值
-# comboxlist = ttk.Combobox(win, textvariable=comvalue)  # 初始化
-# comboxlist["values"] = ("1", "2", "3", "4")
-# comboxlist.current(0)  # 选择第一个
-# comboxlist.bind("<<ComboboxSelected>>", go)  # 绑定事件,(下拉列表框被选中时，绑定go()函数)
-# comboxlist.pack()
-#
-# win.mainloop()  # 进入消息循环
